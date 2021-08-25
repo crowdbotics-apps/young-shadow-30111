0cfc46e31456c50e8e7108711526c1c8427a70ce
Crowdbotics <team@crowdbotics.com>
2021-08-25 21:49:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "young_shadow_30111Identifier",
+  url: "https://young_shadow_30111Identifier.botics.co",
+  api: "https://young_shadow_30111Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
